bd8d4e26f56d23e8e1caef4090d6c8f9d3114540
usatyo <yu0424.math8128@gmail.com>
2022-08-20 00:25:44 +0900

    make pages

---
+++ bridge/src/pages/Home.js
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <div>
+      <div>相手とのやりとり</div>
+      <div>近くの相手一覧</div>
+      <div>取引実績</div>
+      <div>bioの編集？</div>
+      <button>商品登録ボタン</button>
+    </div>
+  )
+}
+
+export default Home
