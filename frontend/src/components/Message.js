9d3fd73b9fe0a7d9b7c73d25dcdf10bd2d88c6ef
Takahashiprog <gab108130@icloud.com>
2022-08-31 00:31:52 +0900

    Co-authored-by: usatyo <usatyo@users.noreply.github.com>

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,16 +1,40 @@
-import { Heading, Box } from "@chakra-ui/react"
+import { Heading, Box, Text, Avatar, HStack, VStack, Spacer } from "@chakra-ui/react"
 
 const Message = () => {
 
-  // TODO chat-ui-kit-react??
+  const chatList = [
+    { "name": "Sapporo Foods", "message": "それはよかったです。" },
+    { "name": "foodjapan-sapporo", "message": "よろしくお願いします。" },
+    { "name": "foodbank sapporo", "message": "お世話になっております。" },
+  ]
 
   return (
     <>
-      <Box>
+      <VStack spacing="30px">
         <Heading size="md">メッセージ</Heading>
-      </Box>
+        <Spacer />
+        {chatList.map((val) => (
+          <Box
+            textAlign="left"
+            width="500px"
+            border='1px'
+            borderColor='black'
+            boxShadow='lg'
+            padding='10px'
+            rounded='md'
+            bg='white'>
+            <HStack spacing="20px" marginLeft="10px">
+              <Avatar />
+              <Box>
+                <Text fontSize={20}>{val["name"]}</Text>
+                <Text fontSize={18} color='gray' mt="2px" ml="10px">{val["message"]}</Text>
+              </Box>
+            </HStack>
+          </Box>
+        ))}
+      </VStack>
     </>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
